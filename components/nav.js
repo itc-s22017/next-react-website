d49d796322461275f891adce7ec390eef60a9c0f
itc-s22017 <s22017@std.it-college.ac.jp>
2022-12-22 09:53:59 +0000

    first commit

---
+++ components/nav.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import styles from 'styles/nav.module.css'
+
+export default function Nav () {
+  return (
+    <nav>
+      <ul className={styles.list}>
+        <li>
+          <Link legacyBehavior href='/'>Home</Link>
+        </li>
+        <li>
+          <Link legacyBehavior href='/about'>About</Link>
+        </li>
+        <li>
+          <Link legacyBehavior href='/blog'>Blog</Link>
+        </li>
+      </ul>
+    </nav>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Hero from 'components/hero'
+const Home = () => {
+  return (
+    <>
+      <Hero
+        title='CUBE'
+        subtitle='アウトプットしていくサイト'
+        imageOn
+      />
+
+    </>
+  )
+}
+
+export default Home
